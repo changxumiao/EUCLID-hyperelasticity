d07461af9a268b0c0db2f33445ea6fbfa5664632
changxumiao <changxumiao@yahoo.com>
2021-10-15 15:16:19 +0800

    test2

--- drivers/main.py
+++ drivers/main.py
@@ -1,41 +1,43 @@
-#=====================================================================
+# =====================================================================
 # INITIALIZATIONS:
-#=====================================================================
-#sys and core
+# =====================================================================
+# sys and core
 import sys
+
 sys.path.append(".")
 
 from core import *
-#config
+# config
 import config as c
-#CUDA
+
+# CUDA
 initCUDA(c.cuda)
 
 datasets = []
 counter_load = -1
 for loadstep in c.loadsteps:
-    counter_load +=1
-#=====================================================================
-# DATA:
-#=====================================================================
-    data = loadFemData(c.femDataPath+'/'+str(loadstep), AD=True, noiseLevel = c.noiseLevel, noiseType = 'displacement')
+    counter_load += 1
+    # =====================================================================
+    # DATA:
+    # =====================================================================
+    data = loadFemData(c.femDataPath + '/' + str(loadstep), AD=True, noiseLevel=c.noiseLevel, noiseType='displacement')
     datasets.append(data)
     data.convertToNumpy()
-#=====================================================================
-# LHS & RHS OF THE WEAK FORM:
-#=====================================================================
+    # =====================================================================
+    # LHS & RHS OF THE WEAK FORM:
+    # =====================================================================
     numFeatures = getNumberOfFeatures()
     if counter_load == 0:
-        LHS = np.zeros([numFeatures,numFeatures])
+        LHS = np.zeros([numFeatures, numFeatures])
         RHS = np.zeros([numFeatures])
-    LHS , RHS = extendLHSRHS(data,c,LHS,RHS)
-#=====================================================================
-# Lp-NORM PENALTY:
-#=====================================================================
+    LHS, RHS = extendLHSRHS(data, c, LHS, RHS)
+    # =====================================================================
+    # Lp-NORM PENALTY:
+    # =====================================================================
     if loadstep == c.loadsteps[-1]:
-        theta = applyPenaltyLpIteration(datasets,LHS,RHS,c)
-#=====================================================================
-# PRINT RESULTS:
-#=====================================================================
-        saveResultsLp(theta,c,counter_load=counter_load)
+        theta = applyPenaltyLpIteration(datasets, LHS, RHS, c)
+        # =====================================================================
+        # PRINT RESULTS:
+        # =====================================================================
+        saveResultsLp(theta, c, counter_load=counter_load)
     c.penaltyLp = c.penaltyLp_init
